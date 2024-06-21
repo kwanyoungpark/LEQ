217d2c829274f9648feea8b5e692a997613f4c32
Kwanyoung Park <williampark202@gmail.com>
2024-06-21 22:11:37 +0900

    Remove unused config

--- configs/config.py
+++ configs/config.py
@@ -7,11 +7,9 @@ def get_config():
     lr = 1e-4
     actor_lr = 3e-5
     config.actor_lr = actor_lr
-    config.alpha_lr = lr
     config.value_lr = lr
     config.critic_lr = lr
 
-    config.temperature = 0.0
     config.dropout_rate = None
 
     ## World Model
